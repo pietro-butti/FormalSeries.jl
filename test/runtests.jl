1c9210a1ec809f37673947a53752ecef8e782c48
David Albandea PROX15-2 <albandeadavid@gmail.com>
2024-02-27 15:51:10 +0100

    beautify tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,29 +9,34 @@
 ### created: Thu Jun 30 16:17:07 2022
 ###                               
 
+using Test
 
-include("test1.jl")
-include("test2.jl")
-include("test3.jl")
-include("test4.jl")
-include("test5.jl")
-include("test6.jl")
-include("test7.jl")
-include("test8.jl")
-include("test18.jl")
-include("test19.jl")
-include("test20.jl")
+@testset verbose = true "one-dimensional series" begin
+    include("test1.jl")
+    include("test2.jl")
+    include("test3.jl")
+    include("test4.jl")
+    include("test5.jl")
+    include("test6.jl")
+    include("test7.jl")
+    include("test8.jl")
+    include("test18.jl")
+    include("test19.jl")
+    include("test20.jl")
+end
 
 ##
 # Test set for d-dimansional series
 ##
 
-include("test9.jl")
-include("test10.jl")
-include("test11.jl")
-include("test12.jl")
-include("test13.jl")
-include("test14.jl")
-include("test15.jl")
-include("test16.jl")
-include("test17.jl")
+@testset verbose = true "d-dimensional series" begin
+    include("test9.jl")
+    include("test10.jl")
+    include("test11.jl")
+    include("test12.jl")
+    include("test13.jl")
+    include("test14.jl")
+    include("test15.jl")
+    include("test16.jl")
+    include("test17.jl")
+end
